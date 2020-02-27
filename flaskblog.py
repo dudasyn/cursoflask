777f3a27f7e674038436bde8c54a955fc4fa2a84
Eduardo de Sant'Ana <lemmoncollection@gmail.com>
2020-02-27 11:10:01 -0300

    Terminei a parte 3 - visão geral sobre posts e templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'e98ead1e4ad5fa28cdff08a3cccd4e7b'
 
 # Some dumb data
 posts = [
@@ -23,13 +26,36 @@ posts = [
 ]
 
 @app.route("/")
+
 @app.route("/home")
 def home():
-    return render_template('home.html',posts=posts,title="PASSING")
-    
+    return render_template('home.html', posts=posts)
+
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
